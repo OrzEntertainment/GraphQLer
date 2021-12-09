fc631aa89dd9c0593f4387ff7a429a3f358e49e9
xiaojianrui <1124942792@qq.com>
2021-12-08 21:50:29 -0800

    edit orchestrator.py

--- utils/orchestrator.py
+++ utils/orchestrator.py
@@ -4,16 +4,48 @@ James
 """
 
 
+from typing import List
+
+from utils.requester import render_seq
+
+
 class Orchestrator:
-    graph = None
+    
 
     # constructor
-    def __init__(self, graph):
+    def __init__(self, graph=None, maxLen=None, bugSequences=None):
         self.graph = graph
-
+        self.maxLen = maxLen
+        self.bugSequences = []
     # main orchestrate function
-    def orchestrate(self):
+    def orchestrate(self) -> List[request]:
+        for node in self.graph.nodes():
+            if self.graph.out_degrees(node) == 0:
+                dfs(self, [], node, self.maxLen)
         """
         TODO: main method to run
         """
-        pass
+
+    def dfs(self, sequence, node, n) -> None:
+        if(len(sequence) > n):
+            return
+        
+        [validSeq, bugSeq] = render_seq(sequence)
+
+        for bSeq in bugSeq:
+            self.bugSequences.append(bSeq)
+
+        for seq in validSeq:
+            for node in self.graph:
+                if self.graph.out_degree(node) == 0:
+                    deletedEdges = self.graph.in_edges(nbunch=node)
+                    seq.append(node)
+                    self.graph.remove(node)
+                    self.dfs(self, seq, node)
+                    seq.remove(node)
+                    self.graph.add_edges_from(deletedEdges)
+        
+        
+        
+
+    
